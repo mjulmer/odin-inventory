dd9617aa5c9ada06e0d0a4765a6be65d285bcf92
M Ulmer <mju9999@gmail.com>
2025-08-10 14:08:42 -0700

    Add homepage with list of all categories

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 const express = require("express");
 const categoryRouter = require("./routes/categoryRouter.ts");
 const itemRouter = require("./routes/itemRouter.ts");
+const inventoryDb = require("./db/dataLayer.ts");
 const app = express();
 
 app.set("view engine", "pug");
@@ -8,7 +9,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/category", categoryRouter);
 app.use("/item", itemRouter);
-app.use("/", (req, res) => res.send("Hit the express server."));
+app.use("/", async (req, res) => {
+  const categories = await inventoryDb.getCategories();
+  res.render("homepage", { categories });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}`));
